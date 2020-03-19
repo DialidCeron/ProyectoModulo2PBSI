c6954eccc42e61cbd9c23b3612d896d552817262
luisdavid <luisdaviduacm@gmail.com>
2020-03-19 16:09:56 -0600

    Se corrigieron algunos problemas de permisos

--- actualizarDrupal.py
+++ actualizarDrupal.py
@@ -43,17 +43,19 @@ argumentos=parse().__dict__
 if __name__ == '__main__':
 	ruta=argumentos["ruta"]
 	if(path.exists(ruta)):
-		if(not path.exists(home+"/.config/composer/vendor/drush/drush")):
+		if(system("drush version 1>/tmp/null 2>/tmp/null") != 0):
 			print("No existe Drush")
 			print("Instalando Drush ...")
-			s = popen("\
+			out=popen("\
 				curl -sS https://getcomposer.org/installer | php7.3;\
 				sudo mv composer.phar /usr/local/bin/composer;\
 				composer global require drush/drush:8.x;\
-				sudo echo 'export PATH=$PATH:$HOME/.config/composer/vendor/drush/drush' >> /etc/bash.bashrc;\
-				source /etc/bash.bashrc;\
+				sudo set -i '$a  export PATH=$PATH:$HOME/.config/composer/vendor/drush/drush/drush' /etc/bash.bashrc\
 				").readlines()
-			print("Se ha instalado Drush",s)
+			print("Se ha instalado Drush",out)
+			print("Ahora basta con escribir el comando: source /etc/bash.bashrc")
+			print("Para poder volver a ejecutar este programa de nuevo.")
+			exit()
 		if(not path.isfile("/usr/bin/git")):
 			print("No existe Git")
 			print("Instalando Git")
@@ -76,23 +78,24 @@ if __name__ == '__main__':
 		print("------------------------------------------------------")
 
 		sitiosRespaldados={}
-		for sitio in sitiosDrupal:
-			print("-----> Respaldando sitio "+sitio)
-			rutaSitio=path.join(ruta,sitio)
-			if comprimirDirectorio(rutaSitio):
-				print("El sitio {0} se comprimio de forma correcta.".format(sitio))
-				if (system("cd "+rutaSitio+"; drush -v sql-dump --result-file="+\
-					path.join(directorioActual,"backup-"+sitio+"_"+\
-						datetime.now().strftime("%d-%m-%Y_%H:%M:%S")+".sql > /tmp/null"))==0):
-					print("Se realizo el respaldo de la base de datos correctamente del sitio: "+sitio)
-					sitiosRespaldados[sitio]=True
-				else:
-					print(">>>>>> No se pudo realizar el resplado de la base de datos  <<<<<<<<")
-					sitiosRespaldados[sitio]=False
-
-		
-
-
-
+		if(len(sitiosDrupal)>0):
+			for sitio in sitiosDrupal:
+				print("----------------> Respaldando sitio <----------------"+sitio)
+				rutaSitio=path.join(ruta,sitio)
+				if comprimirDirectorio(rutaSitio):
+					print("El sitio {0} se comprimio de forma correcta.".format(sitio))
+					if (system("cd "+rutaSitio+"; drush -v sql-dump --result-file="+\
+						path.join(directorioActual,"backup-"+sitio+"_"+\
+							datetime.now().strftime("%d-%m-%Y_%H:%M:%S")+".sql"))==0):
+						print("Se realizo el respaldo de la base de datos correctamente del sitio: "+sitio)
+						print()
+						sitiosRespaldados[sitio]=True
+					else:
+						print(">>>>>> No se pudo realizar el respaldo de la base de datos  <<<<<<<<")
+						print()
+						sitiosRespaldados[sitio]=False
+		else:
+			print("No se encontraron sitios Drupal")
+	
 	else:
 		print("El directorio que ha ingresado no existe o no se puede acceder.")
