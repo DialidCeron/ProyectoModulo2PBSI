b49ff30415c9110b516408d31b0bc57de62690bc
Dialid <dialid.ceron@gmail.com>
2020-03-20 16:58:34 -0600

    correcion

--- obtenerConfiguracionApache.sh
+++ obtenerConfiguracionApache.sh
@@ -14,8 +14,8 @@ cp /etc/apache2/sites-available/drupal2.conf .
 cp /etc/apache2/apache2.conf .
 cp /etc/apache2/conf-available/security.conf .
 
-cp /etc/ssl/certs/drupal.crt
-cp /etc/ssl/private/drupal.key
+cp /etc/ssl/certs/drupal.certs .
+cp /etc/ssl/private/drupal.key .
 
 tar -czvf configuracionApache.tar.gz .htaccess1 .htaccess2 drupal1.conf drupal2.conf apache2.conf security.conf
 rm .htaccess1 .htaccess2 drupal1.conf drupal2.conf security.conf apache2.conf drupal.crt drupal.key
