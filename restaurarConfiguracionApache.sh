5544616f84ab53bf5d065dac13106a8a14fbf3b4
Dialid <dialid.ceron@gmail.com>
2020-03-20 15:11:33 -0600

    Habilitar sitios

--- restaurarConfiguracionApache.sh
+++ restaurarConfiguracionApache.sh
@@ -13,11 +13,16 @@ mv .htaccess2 /var/www/drupal2/.htaccess
 mv drupal1.conf /etc/apache2/sites-available
 mv drupal2.conf /etc/apache2/sites-available
 
+a2ensite drupal1.conf
+a2ensite drupal2.conf
+
 mv apache2.conf /etc/apache2/
 mv security.conf /etc/apache2/conf-available
 
 mv drupal.crt /etc/ssl/certs/
 mv drupal.key /etc/ssl/private
+
+systemctl restart apache2
 
 echo -e "$Verde \n Configuración de Apache restablecida $Color_Off"
 
