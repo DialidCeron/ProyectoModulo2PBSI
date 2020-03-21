8ff27985e67b6bfc290c355a88598c7ae2751191
oscar011 <oscar11011996@hotmail.com>
2020-03-20 19:34:49 -0600

    Update deshabilitarModulos.sh

--- deshabilitarModulos.sh
+++ deshabilitarModulos.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Deshabilitando modulos sitio1."
-cd /var/www/site1
+cd /var/www/drupal1
 #primero lista los modulos activos, luego los deshabilita
 drush pml --no-core --type=module --status=enabled --pipe | xargs drush -y dis
 
 echo "Deshabilitando modulos sitio2."
-cd /var/www/site2
+cd /var/www/drupal2
 drush pml --no-core --type=module --status=enabled --pipe | xargs drush -y dis
