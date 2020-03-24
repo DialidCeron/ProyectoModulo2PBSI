23750b38dabe1546ef80d1289f98e1a8280ef320
luis <luis@debianbd.becarios.cert.unam.mx>
2020-03-24 16:54:25 -0600

    Se valido la recuperacion de los archivos de configuracion

--- obtenerConfiguracionApache.sh
+++ obtenerConfiguracionApache.sh
@@ -17,7 +17,17 @@ cp /etc/apache2/conf-available/security.conf .
 cp /etc/ssl/certs/drupal.crt .
 cp /etc/ssl/private/drupal.key .
 
-tar -czvf configuracionApache.tar.gz .htaccess1 .htaccess2 drupal1.conf drupal2.conf apache2.conf security.conf
-rm .htaccess1 .htaccess2 drupal1.conf drupal2.conf security.conf apache2.conf drupal.crt drupal.key
+tar -czvf configuracionApache.tar.gz .htaccess1 .htaccess2 drupal1.conf drupal2.conf apache2.conf security.conf drupal.crt drupal.key
 
-echo -e "$Verde \n Configuración de Apache obtenida $Color_Off"
+if [ $? -ne 0 ]; then
+	echo "Error: verifique sus permisos"
+	if [ -e configuracionApache.tar.gz ]; then
+		rm configuracionApache.tar.gz
+	fi 
+else
+	rm .htaccess1 .htaccess2 drupal1.conf drupal2.conf security.conf apache2.conf drupal.crt drupal.key
+
+	echo -e "$Verde \n Configuración de Apache obtenida $Color_Off"
+fi
+
+
